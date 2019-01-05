f5e514804870762364e975d67a2b717da25b3d9a
Tristan Bagot <tristan.bagot@gmail.com>
2019-01-05 20:55:33 +0100

    analytics

---
+++ www/site/snippets/analytics.php
@@ -0,0 +1,11 @@
+<?php if($site->googleanalytics()->isNotEmpty()): ?>
+  	<script>
+	window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+	ga('create', '<?= $site->googleanalytics() ?>', 'auto');
+	ga('require', 'autotrack');
+	ga('send', 'pageview');
+	</script>
+	<script async src='https://www.google-analytics.com/analytics.js'></script>
+	<script async src='
+	https://cdnjs.cloudflare.com/ajax/libs/autotrack/0.6.5/autotrack.js'></script>
+<?php endif ?>
