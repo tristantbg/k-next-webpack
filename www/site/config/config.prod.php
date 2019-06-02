b07b01ead4b7eca523e19d7988df55f3edb00c11
Tristan Bagot <tristan.bagot@gmail.com>
2019-06-02 14:38:14 +0200

    ok

--- www/site/config/config.prod.php
+++ www/site/config/config.prod.php
@@ -8,6 +8,10 @@ return [
     'environnement' => 'prod',
     'thumbs'        => [
         'driver' => 'im',
+        'srcsets' => [
+            'default' => [200, 400, 600, 1024, 1600, 1920],
+            'cover'   => [800, 1024, 2048, 4000],
+        ],
     ],
     'hooks'         => [
         'page.create:after' => function ($page) {
